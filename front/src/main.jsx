fb9c33247749ce022395c7661939c15322c7d1a4
Atori Ikeyama <atori.ikeyama@gmail.com>
2024-12-14 11:58:36 +0900

    Refactor imports to streamline React usage and adjust Splash page styles for full viewport width

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Home from './pages/Home.jsx';
